0022bcd9f1d5aa6d2a51e00f17b784a3b63711d9
Seymu1R <102611811+Seymu1R@users.noreply.github.com>
2022-04-14 17:37:14 +0400

    first commit

---
+++ Ededin_reqemlerinin_ceminin _tapilmasi/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ededin_reqemlerinin_ceminin__tapilmasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please,include number ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int remainder = 0;
+            int total = 0;
+            
+            while (number>0)
+            {
+                remainder = number % 10;
+                total += remainder;
+                number /= 10;
+                
+            }
+            Console.WriteLine("Sum of digits  "+total);
+
+        }
+               
+              
+        
+
+    }
+}
